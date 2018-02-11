8976aca9c169706a08b55455afa632037e534e2f
Angela Wang <ranran.wang@wework.com>
2018-02-11 18:46:17 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,10 @@
 # Write your #display_rainbow method here
 
 def #display_rainbow(colors)
-  puts "#{colors[0][0]}: #{colors[0]}, #{colors[1][0]}: #{colors[1]}, #{colors[2][0]}: #{colors[2]},  "
+  puts "#{colors[0][0]}: #{colors[0]}," 
+  puts "#{colors[1][0]}: #{colors[1]},"
+  puts "#{colors[2][0]}: #{colors[2]},"
+  puts "#{colors[3][0]}: #{colors[3]},"
+  puts "#{colors[4][0]}: #{colors[4]},"
+  puts "#{colors[5][0]}: #{colors[5]},"
+  puts "#{colors[6][0]}: #{colors[6]}"
